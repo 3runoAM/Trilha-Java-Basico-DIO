917f0a646bab2e9ea093ff9bc1868283d49a9778
Bruno Martins <jbrunno17@gmail.com>
2024-09-02 10:07:56 -0300

    Resolve exercício Lista de tarefas

---
+++ Exercicios/Set/PesquisaComSet/TaskListExample/Task.java
@@ -0,0 +1,32 @@
+package TaskListExample;
+
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setComplete() {
+        this.isDone = true;
+    }
+
+    public void setIncomplete(){
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return "Taks: [Description: %s, Completed: %s]".formatted(description, isDone);
+    }
+}
---
+++ Exercicios/Set/PesquisaComSet/TaskListExample/TaskList.java
@@ -0,0 +1,63 @@
+package TaskListExample;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class TaskList {
+    private static Set<Task> taskList = new HashSet<Task>();
+
+    public static Set<Task> getTaskList() {
+        return taskList;
+    }
+    /**/
+
+    public static void addTask(Task task) {
+        taskList.add(task);
+    }
+
+    public static void removeTask(Task task) {
+        taskList.removeIf(t -> t.getDescription().equalsIgnoreCase(task.getDescription()));
+    }
+
+    public static void showTaskList() {
+        taskList.forEach(System.out::println);
+    }
+
+    public static int countTasks(){
+        return taskList.size();
+    }
+
+    public static Set<Task> getCompleteTasks() {
+        return taskList.stream()
+                .filter(Task::isDone)
+                .collect(Collectors.toSet());
+    }
+
+    public static Set<Task> getIncompleteTasks() {
+        return taskList.stream()
+                .filter(t -> !t.isDone())
+                .collect(Collectors.toSet());
+    }
+
+    public static void completeTask(String description) {
+        Optional<Task> taskOp = taskList.stream()
+                .filter(t -> t.getDescription().equalsIgnoreCase(description))
+                .findFirst();
+
+        taskOp.ifPresent(Task::setComplete);
+    }
+
+    public static void incompleteTask(String description) {
+        Optional<Task> taskOp = taskList.stream()
+                .filter(t -> t.getDescription().equalsIgnoreCase(description))
+                .findFirst();
+
+        taskOp.ifPresent(Task::setIncomplete);
+    }
+
+    public static void removeAllTasks() {
+        taskList.clear();
+    }
+}
